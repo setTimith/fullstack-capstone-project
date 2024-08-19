6d48a5249fdf707b07487ee31cd303e8a77c18a9
Thomas A <thomasabboud0@gmail.com>
2024-08-19 16:42:27 -0400

    Connect Express

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -15,14 +15,20 @@ async function connectToDatabase() {
 
     const client = new MongoClient(url);      
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    try {
+        // Task 1: Connect to MongoDB
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+        // Task 2: Connect to database giftdb and store in variable dbInstance
+        dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+        // Task 3: Return database instance
+        return dbInstance;
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+        throw err;
+    }
 }
 
 module.exports = connectToDatabase;
