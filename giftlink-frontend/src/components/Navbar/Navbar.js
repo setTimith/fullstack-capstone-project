e94014545f49808392d94388c4c7d1eed29ea121
Thomas A <thomasabboud0@gmail.com>
2024-08-21 17:33:55 -0400

    bugfix

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -35,7 +35,7 @@ export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
-        <a className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</a>
+        <a className="navbar-brand" href={`/app`}>GiftLink</a>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
